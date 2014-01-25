a862319c01867df066dc325603330e24a98df1fc
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 19:53:10 +0100

    post formats

--- functions.php
+++ functions.php
@@ -13,10 +13,10 @@ if (function_exists('add_theme_support')) {
 
     /*
 	 * Enable support for Post Formats.
-	 * See http://codex.wordpress.org/Post_Formats
+     * 'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery', 'status', 'chat'
 	 */
 	add_theme_support( 'post-formats', array(
-		'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery', 'status', 'chat'
+		'quote'
 	) );
 
     // Add Thumbnail Theme Support
