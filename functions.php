5b2f75a8ea3026771dbdbb0035c324581f752fea
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 15:44:32 +0100

    add: sidebar support

--- functions.php
+++ functions.php
@@ -1,4 +1,17 @@
 <?php 
 
+// If Dynamic Sidebar Exists
+if (function_exists('register_sidebar')) {
+    // Define Sidebar Widget Area 1
+    register_sidebar(array(
+        'name' => 'Sidebar 1',
+        'description' => 'Description for this widget-area...',
+        'id' => 'sidebar-1',
+        'before_widget' => '<div id="%1$s" class="%2$s">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>'
+    ));
+}
 
 ?>
