1c70b3d46d48a2c5db7402c7e64a608c2b750bb7
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 14:48:27 +0100

    mod: header cleanup

--- header.php
+++ header.php
@@ -2,26 +2,17 @@
 <html <?php language_attributes(); ?> class="no-js">
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+		<meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-		<link href="//www.google-analytics.com" rel="dns-prefetch">
+		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' :'; } ?> <?php bloginfo('name'); ?></title>
+		<meta name="description" content="<?php bloginfo('description'); ?>">
+
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
         <link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
 
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
 		<?php wp_head(); ?>
-		<script>
-        // conditionizr.com
-        // configure environment tests
-        conditionizr.config({
-            assets: '<?php echo get_template_directory_uri(); ?>',
-            tests: {}
-        });
-        </script>
-
 	</head>
 	<body <?php body_class(); ?>>
 
@@ -34,7 +25,6 @@
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
-							<!-- svg logo - toddmotto.com/mastering-svg-use-for-a-retina-web-fallbacks-with-png-script -->
 							<img src="<?php echo get_template_directory_uri(); ?>/img/logo.svg" alt="Logo" class="logo-img">
 						</a>
 					</div>
@@ -42,7 +32,24 @@
 
 					<!-- nav -->
 					<nav class="nav" role="navigation">
-						<?php html5blank_nav(); ?>
+						<? wp_nav_menu(array(
+							'theme_location'  => 'header-menu',
+							'menu'            => '',
+							'container'       => 'div',
+							'container_class' => 'menu-{menu slug}-container',
+							'container_id'    => '',
+							'menu_class'      => 'menu',
+							'menu_id'         => '',
+							'echo'            => true,
+							'fallback_cb'     => 'wp_page_menu',
+							'before'          => '',
+							'after'           => '',
+							'link_before'     => '',
+							'link_after'      => '',
+							'items_wrap'      => '<ul>%3$s</ul>',
+							'depth'           => 0,
+							'walker'          => ''
+						)); ?>
 					</nav>
 					<!-- /nav -->
 
