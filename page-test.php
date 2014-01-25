de331ece86d73c9987eb6626749c4ee87bdbcb40
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 16:41:10 +0100

    mod: page

--- page-test.php
+++ page-test.php
@@ -1,14 +1,20 @@
+/*
+Template Name: Custom Page Test
+*/
+
 <?php get_header(); ?>
 
 <?php get_sidebar(); ?>
 
 	<main role="main">
+		<!-- section -->
 		<section>
 
 			<h1><?php the_title(); ?></h1>
 
 		<?php while (have_posts()) : the_post(); ?>
 
+			<!-- article -->
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 				<?php the_content(); ?>
