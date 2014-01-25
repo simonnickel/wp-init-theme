29400c5b62354e66f806dc51ef10b43501852325
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 20:02:20 +0100

    add: 404

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+	<section class="error-404">
+
+		<header class="page-header">
+			<h1 class="page-title">Not found</h1>
+		</header>
+
+		<?php get_search_form(); ?>
+
+	</section> <!-- /index-section -->
+
+<?php get_footer(); ?>
