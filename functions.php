cbd0f51d426c6049d0934e094a4d31c86d9d57f3
Simon Nickel <simonnickel@googlemail.com>
2014-01-25 15:50:06 +0100

    add: theme support settings

--- functions.php
+++ functions.php
@@ -1,4 +1,42 @@
-<?php 
+<?php
+
+if (function_exists('add_theme_support')) {
+    // Add Menu Support
+    add_theme_support('menus');
+
+    // Enables post and comment RSS feed links to head
+    add_theme_support('automatic-feed-links');
+
+    /*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
+	add_theme_support( 'post-formats', array(
+		'aside', 'image', 'video', 'audio', 'quote', 'link', 'gallery',
+	) );
+
+    // Add Thumbnail Theme Support
+    add_theme_support('post-thumbnails');
+    add_image_size('large', 700, '', true); // Large Thumbnail
+    add_image_size('medium', 250, '', true); // Medium Thumbnail
+    add_image_size('small', 120, '', true); // Small Thumbnail
+    add_image_size('custom-size', 700, 200, true); // Custom Thumbnail Size call using the_post_thumbnail('custom-size');
+
+	// Add support for featured content.
+	/*add_theme_support( 'featured-content', array(
+		'featured_content_filter' => 'twentyfourteen_get_featured_posts',
+		'max_posts' => 6,
+	) );
+	*/
+	// This theme uses its own gallery styles.
+	//add_filter( 'use_default_gallery_style', '__return_false' );
+
+    // Add Support for Custom Backgrounds - Uncomment below if you're going to use
+    /*add_theme_support('custom-background', array(
+	'default-color' => 'FFF',
+	'default-image' => get_template_directory_uri() . '/img/bg.jpg'
+    ));*/
+}
 
 // If Dynamic Sidebar Exists
 if (function_exists('register_sidebar')) {
